4767b3f8968f2941330c12007f5c1e43cd491452
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2024-03-26 09:26:24 -0300

    About View

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 
@@ -22,20 +31,36 @@ public class MainViewController implements Initializable{
 	public void onMenuItemSellerAction() { 
 		System.out.println("onMenuItemSellerAction");
 	}
-	@FXML
 	
+	@FXML
 	public void onMenuItemDepartmentAction() { 
 		System.out.println("onMenuItemDepartmentAction");
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() { 
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
 	
 	@Override
-	public void initialize(URL url, ResourceBundle rb) {
+	public void initialize(URL url, ResourceBundle rb) {}
 
+	
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent(); // pega o primeiro elemento da view, acessa o conteudo do ScroolPane,
+			
+			Node mainMenu = mainBox.getChildren().get(0); // primeiro filho da janela principal
+			mainBox.getChildren().clear();
+			mainBox.getChildren().add(mainMenu);
+			mainBox.getChildren().addAll(newVBox.getChildren());
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
-
 }
